5c304478c5ef2d912edc238e609eb4a0d8aafca2
Bill Cousins <bcousins@enable.services>
2018-12-03 17:11:12 +0000

    Day3 in the books

--- day3/part2.js
+++ day3/part2.js
@@ -2,9 +2,11 @@ module.exports = function(claims) {
     var xCoord = [];
     var yCoord = [];
     var regex = /#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)/;
+    var ids = [];
 
     for(i=0;i<claims.length;i++) {
         var claim = regex.exec(claims[i]);
+        ids.push(claim[1]);
         for (x=claim[2];x<parseInt(claim[2]) + parseInt(claim[4]);x++) {
             if (!xCoord[x]) {
                 xCoord[x] = [];
@@ -18,16 +20,20 @@ module.exports = function(claims) {
             yCoord[y].push(claim[1]);
         }
     }
-    var overlaps = 0;
     for (x=0;x<xCoord.length;x++) {
         for (y=0;y<yCoord.length;y++) {
             if (xCoord[x] && yCoord[y]) {
                 var overlapping = xCoord[x].filter(function(n) {return yCoord[y].indexOf(n) > -1;});
                 if (overlapping.length > 1) {
-                    overlaps += 1;
+                    for (i=0;i<overlapping.length;i++) {
+                        var index = ids.indexOf(overlapping[i]);
+                        if (index > -1) {
+                            ids.splice(index, 1);
+                        }
+                    }
                 }
             }
         }
     }
-    console.log(overlaps);
+    console.log(ids.pop());
 };
