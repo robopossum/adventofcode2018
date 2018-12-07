ea018b8dcd1d68a57f8088264447408174d8dee3
Bill Cousins <bcousins@enable.services>
2018-12-07 18:00:25 +0000

    Half done

--- day7/part2.js
+++ day7/part2.js
@@ -14,22 +14,25 @@ module.exports = function (steps) {
         inv[matches[2]] = inv[matches[2]] || '';
         inv[matches[1]] = inv[matches[1]] + matches[2];
     }
+    console.log(objs);
     var length = Object.keys(objs).length;
     var index;
     while (length > 0) {
         for (i=0;i<26;i++) {
             let letter = String.fromCharCode(65 + i);
             if (objs[letter] === '') {
-                let req = inv[letter].split('');
+                let req = objs[letter].split('');
+                console.log(req);
                 let start = 0;
                 for (k=0;k<req.length;k++) {
-                    
+                    start = complete[req[k]] > start ? complete[req[k]] : start;
                 }
                 index = 0;
                 for (j=1;j<5;j++) {
                     index = workers[j] < workers[index] ? j : index;
                 }
-                workers[index] += 61 + i;
+                workers[index] = start + 61 + i;
+                complete[letter] = workers[index];
                 delete objs[letter];
                 length--;
                 for(let key in objs) {
@@ -40,5 +43,6 @@ module.exports = function (steps) {
         }
     }
     console.log(workers);
+    console.log(complete)
     return workers[index];
 };
